f7d42d9a420fe6fa935feb6ff29edb14eadbbbe1
Thomas <123959293+thomas-stecinski@users.noreply.github.com>
2024-05-28 20:04:24 +0200

    ajout des modèles

---
+++ src/main/java/com/example/OlympiqueAPI/OlympiqueApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.OlympiqueAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlympiqueApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlympiqueApiApplication.class, args);
+	}
+
+}
